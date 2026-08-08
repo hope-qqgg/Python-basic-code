7f00f0f08d5fa7273d676de88a5d1a03aa7cd6a4
hope-qqgg <id_1222691@foxmail.com>
2026-08-08 11:32:25 +0800

    第二章 01.字面量与变量

---
+++ 第二章/01.字面量与变量.py
@@ -0,0 +1,10 @@
+print(100)#整数（int）
+print(3.14)#浮点数（float）
+print(True)#布尔值（bool）
+print(False)#布尔值（bool）
+print("Hello World")#字符串（str）
+print("---------------")#字符符（str）
+print(None)#空值（NoneType）
+#布尔类型本质也是整数类型，True = 1，False = 0
+print(True + 1)#2
+print(False - 1)#-1
