fa325824f0abf1d34ead428005156789e405b508
hope-qqgg <id_1222691@foxmail.com>
2026-08-15 23:16:37 +0800

    feat(python): 添加if条件判断练习代码
    - 实现用户登录验证功能，支持admin/666888和root/777888两组账户
    - 添加三角形类型判断案例，可识别等边、等腰和普通三角形
    - 实现北京市居民阶梯电价电费计算功能
    - 完善输入输出交互界面，增强用户体验
    ---------------------------------------------------------
    if条件判断结束

--- 第二章/05.if条件判断.py
+++ 第二章/05.if条件判断.py
@@ -92,3 +92,66 @@ if score >= 60:
 else:
     print("不及格")
 print("------------------------------\n")
+
+# 练习：根据输入用户名、密码进行登录系统（用户名、密码为 admin/666888或 root/777888）
+# 1.定义变量记录正确用户名和密码
+correct_username1 = "admin"
+correct_password1 = "666888"
+correct_username2 = "root"
+correct_password2 = "777888"
+# 2.定义变量记录键盘输入的用户名和密码
+username = input("请输入用户名：")
+password = input("请输入密码：")
+# 3.判断用户名和密码是否正确
+if username == correct_username1 and password == correct_password1:
+    print("登录成功")
+elif username == correct_username2 and password == correct_password2:
+    print("登录成功")
+else:
+    print("登录失败")
+print("------------------------------\n")
+
+"""
+    案例：三角形类型判断：根据输入的三个边的边长（正整数），判定是等边三角形、等腰三角形、普通三角形，还是不能构成三角形。
+        1.构成三角形的条件：两边之和大于第三边
+        2.三角形判定规则：
+            三个边都相等：等边三角形
+            两个边相等：等腰三角形
+            三个边都不相等：普通三角形
+"""
+# 1.定义变量记录键盘输入的三个边的边长
+a = int(input("请输第一条边长a："))
+b = int(input("请输入第二条边长b："))
+c = int(input("请输入第三条边长c："))
+# 2.判断是否能构成三角形
+if a + b > c and a + c > b and b + c > a:
+    # 3.判断三角形类型
+    if a == b == c:
+        print(f"{a}{b}{c}能构成等边三角形")
+    elif a == b or a == c or b == c:
+        print(f"{a}{b}{c}能构成等腰三角形")
+    else:
+        print(f"{a}{b}{c}能构成普通三角形")
+else:
+    print(f"{a}{b}{c}不能构成三角形")
+print("------------------------------\n")
+
+"""
+    北京市居民年度用电电费计算：根据输入的用电度数，计算电费
+        北京市居民电费采用阶梯电价计价方式，所谓阶梯电价是指按照用户消费的电量分段定价，用电价格随用电量增加呈阶梯状逐级递增的一种电价定价机制。
+    阶梯电价规则：
+        第一档：2880度以下，电费单价0.4883元/度
+        第二档：2880-4800度，电费单价0.5383元/度
+        第三档：4800度以上，电费单价0.7883元/度
+"""
+# 1.定义变量记录键盘输入的用电度数
+electricity = int(input("请输入用电度数："))
+# 2.判断用电度数所在的档位，并计算电费
+if electricity < 2880:
+    electricity_bill = electricity * 0.4883
+elif electricity < 4800:
+    electricity_bill = 2880 * 0.4883 + (electricity - 2880) * 0.5383
+else:
+    electricity_bill = 2880 * 0.4883 + 1920 * 0.5383 + (electricity - 4800) * 0.7883
+print(f"您的电费为：{electricity_bill}元")
+print("------------------------------\n")
